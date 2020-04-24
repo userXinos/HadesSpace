74a29792aa5ddfb845996ed6c62097efddb38397
Xinos <userxinos@gmail.com>
2020-04-24 22:00:28 +0700

    refactoring modules

--- generateGameData.js_modules/modules.js
+++ generateGameData.js_modules/modules.js
@@ -1,86 +1,82 @@
-'use scict';
-let obj = {
-    rawData: '',
-    dronesData: '',
-    projectilesData: '',
-}
+
 const mainJs = require('../generateGameData.js')
 
-function generateModules(args){
-    let renameKeys = mainJs.renameKeys
-    let combineObjects = mainJs.combineObjects
-    let obj = args.rawData
-    let projectilesKeys = { // новые ключи для объединения 
-        "Alpha": "AlphaRocket",
-        "Delta": "DeltaRocket",
-        "Omega": "OmegaRocket",
-        "Dart": "DartLauncher",
-        "EMPRocket": "EMPRocket",
-        "HydroRocket": "HydroRocket",
-        "BomberRocket": "BomberLauncher",
-      }
-    let starHeaders = ['EffectDurationx10', 'ActivationDelay', 'ActivationPrep', 'MaxDPSTime', 'APTPIOTTP', 'DisableTime']
-    let projectiles = renameKeys(args.projectilesData, projectilesKeys)
-    let drones = {}
-    for (let i of dronesList) {
-      drones[i] = args.shipsData[i]
-    }
-    obj = combineObjects(obj, drones);
-    obj = combineObjects(obj, projectiles);
-    obj['Recoil']['ShowWSInfo'] = 1
-    obj['Immolation']['ShowWSInfo'] = 1
-    obj['EMPRocket']['ShowWSInfo'] = 1
-    obj = addStarInfo(obj, args.fixValue, starHeaders, 'WS');
-    obj = addStarInfo(obj, args.fixValue, starHeaders, 'BS');
-    obj['MiningBoost']['WhiteStarScore'].unshift(0) // ошибка в таблице, не хватает "0"
-    obj['MiningDrone']['MiningSpeed'] = [29.1, 33.3, 37.5, 41.7, 45.8, 52.2, 58.8, 65.2, 74.1, 85.7]; // хз как считать это
-    return obj;
+function generateModules(args) {
+  let renameKeys = mainJs.renameKeys
+  let combineObjects = mainJs.combineObjects
+  let obj = args.rawData
+  let projectilesKeys = { // новые ключи для объединения 
+    "Alpha": "AlphaRocket",
+    "Delta": "DeltaRocket",
+    "Omega": "OmegaRocket",
+    "Dart": "DartLauncher",
+    "EMPRocket": "EMPRocket",
+    "HydroRocket": "HydroRocket",
+    "BomberRocket": "BomberLauncher",
+  }
+  let starHeaders = ['EffectDurationx10', 'ActivationDelay', 'ActivationPrep', 'MaxDPSTime', 'APTPIOTTP', 'DisableTime']
+  let projectiles = renameKeys(args.projectilesData, projectilesKeys)
+  let drones = {}
+  for (let i of dronesList) {
+    drones[i] = args.shipsData[i]
+  }
+  obj = combineObjects(obj, drones);
+  obj = combineObjects(obj, projectiles);
+  obj['Recoil']['ShowWSInfo'] = 1
+  obj['Immolation']['ShowWSInfo'] = 1
+  obj['EMPRocket']['ShowWSInfo'] = 1
+  obj = addStarInfo(obj, args.fixValue, starHeaders, 'WS');
+  obj = addStarInfo(obj, args.fixValue, starHeaders, 'BS');
+  obj['MiningBoost']['WhiteStarScore'].unshift(0) // ошибка в таблице, не хватает "0"
+  obj['Destiny']['WhiteStarScore'].unshift(0) // ошибка в таблице, не хватает "0"
+  obj['MiningDrone']['MiningSpeed'] = [29.1, 33.3, 37.5, 41.7, 45.8, 52.2, 58.8, 65.2, 74.1, 85.7]; // хз как считать это
+  return obj;
 
 }
 // фикс модулей, добавление БЗ/ГЗ стат
 function addStarInfo(obj, fixValue, starHeaders, star) {
-    let isHide = mainJs.isHide
-    let coefficient = (v) => (star == 'WS') ? v * 600 : v * 2
-    let isWhiteListBS = (header, name) => fixValue.whiteListBS.checkList(header, name)
-    let StarHeaders2 = []
-    for (let i = 0; i < starHeaders.length; i++) {
-      StarHeaders2[i] = starHeaders[i] + star
-    }
-    for (let name in obj) {
-      let module = obj[name]
-      let check = obj[name][`Show${star}Info`]
-      delete obj[name][`Show${star}Info`]
-      if (check != 1) continue
-      for (let stata in module) {
-        if (starHeaders.includes(stata)) {
-          if (star == 'BS') { // пока тлоько для ГЗ
-            if (!isWhiteListBS(stata, name)) continue
-          }
-          if (!Object.keys(module).includes(stata + star)) {
-            let stokStata = module[stata]
-            if (!Array.isArray(stokStata)) {
-              obj[name][stata + star] = coefficient(stokStata)
-              if (isHide(name, stata, true)) { // удалить оригинальную стату, если не нужна
-                delete obj[name][stata]
-              }
-              continue
-            }
-            let arrayNoStar = stokStata
-            let arrayStar = []
-            for (let j in arrayNoStar) {
-              arrayStar.push(coefficient(arrayNoStar[j]))
-            }
-            obj[name][stata + star] = arrayStar
+  let isHide = mainJs.isHide
+  let coefficient = (v) => (star == 'WS') ? v * 600 : v * 2
+  let isWhiteListBS = (header, name) => fixValue.whiteListBS.checkList(header, name)
+  let StarHeaders2 = []
+  for (let i = 0; i < starHeaders.length; i++) {
+    StarHeaders2[i] = starHeaders[i] + star
+  }
+  for (let name in obj) {
+    let module = obj[name]
+    let check = obj[name][`Show${star}Info`]
+    delete obj[name][`Show${star}Info`]
+    if (check != 1) continue
+    for (let stata in module) {
+      if (starHeaders.includes(stata)) {
+        if (star == 'BS') { // пока тлоько для ГЗ
+          if (!isWhiteListBS(stata, name)) continue
+        }
+        if (!Object.keys(module).includes(stata + star)) {
+          let stokStata = module[stata]
+          if (!Array.isArray(stokStata)) {
+            obj[name][stata + star] = coefficient(stokStata)
             if (isHide(name, stata, true)) { // удалить оригинальную стату, если не нужна
               delete obj[name][stata]
             }
+            continue
           }
-        } else {
-          continue
+          let arrayNoStar = stokStata
+          let arrayStar = []
+          for (let j in arrayNoStar) {
+            arrayStar.push(coefficient(arrayNoStar[j]))
+          }
+          obj[name][stata + star] = arrayStar
+          if (isHide(name, stata, true)) { // удалить оригинальную стату, если не нужна
+            delete obj[name][stata]
+          }
         }
+      } else {
+        continue
       }
     }
-    return obj
   }
+  return obj
+}
 
-  module.exports = { generateModules }
+module.exports = { generateModules }
--- generateGameData.js_modules/solarSystem.js
+++ generateGameData.js_modules/solarSystem.js
@@ -1,110 +1,99 @@
-'use scict';
+function generateSolarSys(args) {
+  let result = {}
+  let obj = args.rawData
 
-let args = {
-    rawData: '',
-    star: 'yellow',
-    scannersData: '',
-    cerberusData: '',
+  for (let name of Object.keys(obj)) {
+    name = obj[name]
+    for (let key in name) {
+      let value = name[key]
+      let stokValue = result[key]
+      if (stokValue == undefined || stokValue === "") {
+        startObj(key, value, result)
+      } else {
+        if (Array.isArray(stokValue)) {
+          result[key].push(value)
+        } else {
+          result[key] = []
+          result[key].push(stokValue, value)
+        }
+      }
+    }
+    addScannerInfo(name.MinScannerLevel, result, args.scannersData)
+    addCerberus(name.CerbGroup, result, args.cerberusData)
+    endObj(result)
+  }
+  // небольшие фиксы 
+  result['maxLevel'] = result['maxLevel'].length
+  result['Name'] = 'YSS'
+  result['MinScannerLevel'].forEach((e, i, arr) => {
+    if (e !== ' ')
+      arr[i] = e + 1
+  });
+  return result
+}
+function getType(val) {
+  // if(!isNaN(val) && val !== ' '){
+  //   return 0
+  // }
+  return ' '
+}
+function getLength(v) {
+  let r
+  if (v != undefined) {
+    r = v.length
+  }
+  return r || 0
+}
+// добавить записи в начало, для соотвествия уровня
+function startObj(key, value, obj) {
+  let length = getLength(obj['maxLevel'])
+  obj[key] = value
+  if (!length) return
+  obj[key] = [value]
+  for (let k = 0; k < length; k++) {
+    obj[key].unshift(getType(value))
+  }
+}
+// добавить записи в конец, для соотвествия уровня
+function endObj(obj) {
+  let length = getLength(obj['maxLevel'])
+  for (let key in obj) {
+    for (let i = 0; i < length; i++) {
+      if (obj[key].length < length) {
+        obj[key].push(getType(obj[key][0]))
+      } else {
+        continue
+      }
+    }
+  }
+}
+function addScannerInfo(scanner, obj, scanners) {
+  let ststs = ['Cost', 'SectorUnlockTime']
+  if (obj[ststs[0]] == undefined) {
+    for (let i of ststs) {
+      obj[i] = []
+    }
+  }
+  for (let i of ststs) {
+    obj[i].push(scanners[i][scanner])
+  }
+}
+function addCerberus(cerb, obj, cerberus) {
+  if (!cerb) return
+  let cerbObj = (() => {
+    let r = cerberus[cerb]
+    for (let i in r) {
+      let data = r[i]
+      delete r[i]
+      if (!ignoringHeaders.includes(i)) {
+        i = i.replace(/Num(\w*)/, '$1')
+        r[i] = data
+      }
+    }
+    return r
+  })
+  let index = obj.CerbGroup.indexOf(cerb)
+  obj.CerbGroup[index] = cerbObj()
 }
 
-function generateSolarSys(args) {
-    let result = {}
-    let obj = args.rawData
-  
-    for (let name of Object.keys(obj)) {
-      name = obj[name]
-      for (let key in name) {
-        let value = name[key]
-        let stokValue = result[key]
-        if (stokValue == undefined || stokValue === "") {
-          startObj(key, value, result)
-        } else {
-          if (Array.isArray(stokValue)) {
-            result[key].push(value)
-          } else {
-            result[key] = []
-            result[key].push(stokValue, value)
-          }
-        }
-      }
-      addScannerInfo(name.MinScannerLevel, result, args.scannersData)
-      addCerberus(name.CerbGroup, result, args.cerberusData)
-      endObj(result)
-    }
-    // небольшие фиксы 
-    result['maxLevel'] = result['maxLevel'].length
-    result['Name'] = 'YSS'
-    result['MinScannerLevel'].forEach((e, i, arr) => {
-      if (e !== ' ')
-        arr[i] = e + 1
-    });
-    return result
-  }
-  function getType(val) {
-    // if(!isNaN(val) && val !== ' '){
-    //   return 0
-    // }
-    return ' '
-  }
-  function getLength(v) {
-    let r
-    if (v != undefined) {
-      r = v.length
-    }
-    return r || 0
-  }
-  // добавить записи в начало, для соотвествия уровня
-  function startObj(key, value, obj) {
-    let length = getLength(obj['maxLevel'])
-    obj[key] = value
-    if (!length) return
-    obj[key] = [value]
-    for (let k = 0; k < length; k++) {
-      obj[key].unshift(getType(value))
-    }
-  }
-  // добавить записи в конец, для соотвествия уровня
-  function endObj(obj) {
-    let length = getLength(obj['maxLevel'])
-    for (let key in obj) {
-      for (let i = 0; i < length; i++) {
-        if (obj[key].length < length) {
-          obj[key].push(getType(obj[key][0]))
-        } else {
-          continue
-        }
-      }
-    }
-  }
-  function addScannerInfo(scanner, obj, scanners) {
-    let ststs = ['Cost', 'SectorUnlockTime']
-    if (obj[ststs[0]] == undefined) {
-      for (let i of ststs) {
-        obj[i] = []
-      }
-    }
-    for (let i of ststs) {
-      obj[i].push(scanners[i][scanner])
-    }
-  }
-  function addCerberus(cerb, obj, cerberus) {
-    if (!cerb) return
-    let cerbObj = (() => {
-      let r = cerberus[cerb]
-      for (let i in r) {
-        let data = r[i]
-        delete r[i]
-        if (!ignoringHeaders.includes(i)) {
-          i = i.replace(/Num(\w*)/, '$1')
-          r[i] = data
-        }
-      }
-      return r
-    })
-    let index = obj.CerbGroup.indexOf(cerb)
-    obj.CerbGroup[index] = cerbObj()
-  }
-
-  module.exports = {
-    generateSolarSys
-  } 
+module.exports = { generateSolarSys } 
