83893e649349d4b1507ecf9fc6730fcdfd6694c9
Xinos <userxinos@gmail.com>
2020-05-18 21:20:07 +0700

    refactoring plugin modules

--- plugins/modules.js
+++ plugins/modules.js
@@ -4,54 +4,48 @@ const globals = require('./globals.js')
 
 let shipsData = main.readCSV('capital_ships')
 let projectilesData = main.readCSV('projectiles')
-let globalsData = globals.default(main.readCSV('globals'))
 let artifacts = main.readCSV('artifacts')
+let globalsData = globals.default(main.readCSV('globals'))
 let starHeaders = ['EffectDurationx10', 'ActivationDelay', 'ActivationPrep', 'MaxDPSTime', 'APTPIOTTP', 'DisableTime'];
-let projectilesKeys = { // новые ключи для объединения 
-  "Alpha": "AlphaRocket",
-  "Delta": "DeltaRocket",
-  "Omega": "OmegaRocket",
-  "Dart": "DartLauncher",
-  "EMPRocket": "EMPRocket",
-  "HydroRocket": "HydroRocket",
-  "BomberRocket": "BomberLauncher",
-}
 
 exports.default = function (obj) {
-  let projectiles = main.renameKeys(projectilesData, projectilesKeys);
-  let drones = {};
+  for (let key in obj) {
+    let obj1 = obj[key]
 
-  main.dataByTypes.capital_ships.drones.forEach(e => { // собрать дронов по списку 
-    drones[e] = shipsData[e];
-  });
-  obj = main.combineObjects(obj, drones);
-  obj = main.combineObjects(obj, projectiles);
-  ['Recoil', 'Immolation', 'EMPRocket'].forEach(e => obj[e]['ShowWSInfo'] = 1);
+    // добавить глобальные к модулям 
+    Object.keys(globalsData).forEach(e => {
+      if (e.includes(key)) {
+        obj1[e] = globalsData[e]
+      }
+    })
+    // добавить ур. артефакта
+    if (!obj1.Hide) {
+      for (let key2 in artifacts) {
+        let art = artifacts[key2]
+        if (!art.BlueprintTypes) continue;
+        let types = art.BlueprintTypes.split('!')
+        if (art.MaxModuleLevelToAward == obj1.AwardLevel && types.includes(obj1.SlotType)) {
+          obj1.TID_Artifact = art.TID
+        }
+      }
+    }
+    // добавить projectiles к ракетоподобным 
+    if (obj1.WeaponEffectType == 'projectile') {
+      obj[key] = main.combineObjects(obj1, projectilesData[obj1.WeaponFx]);
+    }
+    // добавить данные дронов
+    if (obj1.SpawnLifetime) {
+      obj[key] = main.combineObjects(obj1, shipsData[obj1.Name]);
+    }
+    ['WeaponEffectType', 'WeaponFx', 'Hide'].forEach(e => delete obj1[e]);
+  }
+  ['Recoil', 'Immolation', 'EMPRocket'].forEach(e => obj[e].ShowWSInfo = 1);
   addStarInfo(obj, starHeaders, 'WS');
   addStarInfo(obj, starHeaders, 'BS');
   obj.TimeWarp.Icon = 'Mod_TimeWarp_Icon' // ошибка в таблице, 'w' в иконках в верхнем регистре 
   obj.MiningBoost.WhiteStarScore.unshift(0) // ошибка в таблице, не хватает "0"
   obj.Destiny.WhiteStarScore.unshift(0) // ошибка в таблице, не хватает "0"
   obj.MiningDrone.MiningSpeed = [29.1, 33.3, 37.5, 41.7, 45.8, 52.2, 58.8, 65.2, 74.1, 85.7]; // хз как считать это
-  for (let key in obj) {
-    let obj1 = obj[key]
-    Object.keys(globalsData).forEach(e => {  // добавить глобальные к модулям 
-      if (e.includes(key)) {
-        obj1[e] = globalsData[e]
-      }
-    })
-    let check = obj1.Hide
-    delete obj1.Hide
-    if (check) continue;
-    for (let key2 in artifacts) {// ур артефакта
-      let art = artifacts[key2]
-      if (!art.BlueprintTypes) continue;
-      let types = art.BlueprintTypes.split('!')
-      if (art.MaxModuleLevelToAward == obj1.AwardLevel && types.includes(obj1.SlotType)) {
-        obj1.TID_Artifact = art.TID
-      }
-    }
-  }
   return obj;
 }
 // фикс модулей, добавление БЗ/ГЗ стат
