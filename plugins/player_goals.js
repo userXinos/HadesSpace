d7acf85c2cca1940835360c3524bb750329cb95b
Xinos <userxinos@gmail.com>
2020-06-01 23:44:24 +0700

    assemble "WinBSWith" goals into one object

--- plugins/player_goals.js
+++ plugins/player_goals.js
@@ -2,39 +2,71 @@
 const main = require('../generateGameData.js')
 
 let whiteList = main.dataByTypes.player_goals.all
-exports.default = function (obj) {
+module.exports = function (obj) {
     let newKeys = {
         CRRewardPerDay: 'CRReward',
         FuelRewardPerDay: 'FuelReward'
     }
 
-    for (let k of Object.keys(obj)) {
-        let obj1 = obj[k]
+    Object.keys(obj).forEach(e => { // фикс модулей ГЗ квестов
+        if (e.startsWith('WinBSWith')) {
+            obj[e].module = e.replace(/WinBSWith(.+?)(\d?)$/, '$1!$2')
+        }
+    })
+    for (let key of Object.keys(obj)) {
+        let obj1 = obj[key]
 
         main.fillSpace(obj1, ' ', 'push')
-        if (!whiteList.includes(k)) continue
-        if (k == 'SalvageArtifacts') { // фикс значения "уровень арта", не удобно форматировать и локализировать
+        if (!whiteList.includes(key)) continue
+        if (key == 'SalvageArtifacts') { // фикс значения "уровень арта", не удобно форматировать и локализировать
             obj1.artLevel = []
             obj1.StringParam.forEach((e, i, arr) => {
                 obj1.artLevel.push(e.split('!')[0])
             });
             delete obj1.StringParam
-            obj[k] = obj1
+            obj[key] = obj1
         }
-        if (k.startsWith('WinBSWith')) { // фикс квестов ГЗ
-            obj1.module = k.replace(/WinBSWith(.+?)(\d?)$/, '$1!$2')
-        }
-        for (let i in obj1) {
-            if (Object.keys(newKeys).includes(i)) { // пофиксить награды, тк они равны за 1 день
+        for (let i in obj1) { // пофиксить награды, тк они равны за 1 день
+            if (Object.keys(newKeys).includes(i)) {
                 if (Array.isArray(obj1[i])) {
                     obj1[i].forEach((e, i, arr) => {
-                        arr[i] = e * obj1['TimeLimitDays'][i] || e * obj1['TimeLimitDays']
+                        arr[i] = e * obj1.TimeLimitDays[i] || e * obj1.TimeLimitDays
                     });
                 } else {
-                    obj1[i] = obj1[i] * obj1['TimeLimitDays']
+                    obj1[i] = obj1[i] * obj1.TimeLimitDays
                 }
-                obj[k] = main.renameKeys(obj1, newKeys)
+                obj[key] = main.renameKeys(obj1, newKeys)
             }
+        }
+        if (key.startsWith('WinBSWith')) { // собрать квесты ГЗ в одну таблицу
+            let obj1 = obj[key]
+            for (let i in obj1) {
+                if (obj.WinBSWith == undefined) {
+                    obj.WinBSWith = {}
+                }
+                let WinBSWith = obj.WinBSWith
+
+                if (ignoringHeaders.includes(i) || ['TimeLimitDays', 'GoalTarget'].includes(i)) {
+                    if (i != 'maxLevel') {
+                        WinBSWith[i] = obj1[i]
+                    } else {
+                        if (WinBSWith.maxLevel != undefined) {
+                            WinBSWith.maxLevel++
+                        } else {
+                            WinBSWith.maxLevel = 1
+                        }
+                    }
+                } else {
+                    if (i in obj.WinBSWith) {
+                        WinBSWith[i].push(obj1[i])
+                    } else {
+                        WinBSWith[i] = []
+                        WinBSWith[i].push(obj1[i])
+                    }
+                }
+            }
+            main.fillSpace(obj.WinBSWith, ' ', 'push')
+            delete obj[key]
         }
     }
     return obj
