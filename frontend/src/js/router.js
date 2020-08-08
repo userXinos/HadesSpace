d41043600e5f26ef5950bfbee7dc84800292474f
Xinos <userxinos@gmail.com>
2020-08-08 11:56:14 +0700

    fix bugs, add page 404

--- frontend/src/js/router.js
+++ frontend/src/js/router.js
@@ -100,15 +100,15 @@ export default new Router({
       name: 'YellowStar',
       component: () => import(/* webpackChunkName: "YellowStar" */ '../pages/YellowStar.vue'),
     },
-    // {
-    //   path: '/404',
-    //   name: '404',
-    //   component: () => import(/* webpackChunkName: "404" */ '../pages/404.vue'),
-    // },
-    // {
-    //   path: '*',
-    //   redirect: '/404',
-    // },
+    {
+      path: '/404',
+      name: '404',
+      component: () => import(/* webpackChunkName: "404" */ '../pages/404.vue'),
+    },
+    {
+      path: '*',
+      redirect: '/404',
+    },
   ],
   scrollBehavior(to) {
     if (to.hash) {
