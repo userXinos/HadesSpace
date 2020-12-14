5f4f9b155090222813e5f14578963c627abf0a18
Xinos <userxinos@gmail.com>
2020-12-14 10:41:07 +0700

    styling the table

--- backend/images/table/index.js
+++ backend/images/table/index.js
@@ -1,13 +1,22 @@
 const {parse} = require('node-html-parser');
 
-module.exports = function(html, data) {
+module.exports = function(html, data, opts) {
   const document = parse(html);
-  renderTable(document, data);
+  renderTable(document, data, opts.lang);
+  document.querySelector('table').setAttribute('class', 'theme-' + opts.theme);
   return document.toString();
 };
 
-function renderTable(document, obj) {
+function renderTable(document, data, lang) {
+  const obj = {
+    lvl: Array.from(
+        {length: data.maxLevel},
+        (_, i) => i + 1,
+    ),
+    ...data,
+  };
   const arraysKeys = Object.keys(obj).filter((k) => Array.isArray(obj[k]));
+  const bodyData = arraysKeys.map((e) => rowspanMask(obj[e], true));
   const $thead = document.querySelector('thead');
   const $tbody = document.querySelector('tbody');
 
@@ -20,8 +29,13 @@ function renderTable(document, obj) {
 
   for (let i = 0; i < obj.maxLevel; i++) {
     let result = '';
-    arraysKeys.forEach((key) => {
-      result += '<td>' + obj[key][i] + '</td>';
+    arraysKeys.forEach((k, keyIndex) => {
+      const num = bodyData[keyIndex][i].rowspan;
+      const value = bodyData[keyIndex][i].value;
+      const rowspan = (num) ? `rowspan="${num}" class="rowspaned"` : '';
+      if (value != null) {
+        result += `<td ${rowspan}>` + bodyData[keyIndex][i].value + '</td>';
+      }
     });
     $tbody.insertAdjacentHTML(
         'beforeend',
@@ -30,25 +44,25 @@ function renderTable(document, obj) {
   }
 }
 
-// function rowspanMask(rawArray, mergeCells) {
-//   return rawArray
-//       .map((item, index, arr) => {
-//         if (item == arr[index - 1] && mergeCells) {
-//           item = null;
-//         }
-//         return {value: item};
-//       })
-//       .map((item, index, arr) => {
-//         if (item.value === null) {
-//           let rowspan = 1;
-//           let lastItemIndex = index;
+function rowspanMask(rawArray, mergeCells) {
+  return rawArray
+      .map((item, index, arr) => {
+        if (item == arr[index - 1] && mergeCells) {
+          item = null;
+        }
+        return {value: item};
+      })
+      .map((item, index, arr) => {
+        if (item.value === null) {
+          let rowspan = 1;
+          let lastItemIndex = index;
 
-//           while (arr[lastItemIndex].value == null) {
-//             lastItemIndex--;
-//             rowspan++;
-//           }
-//           arr[lastItemIndex].rowspan = rowspan;
-//         }
-//         return item;
-//       });
-// }
+          while (arr[lastItemIndex].value == null) {
+            lastItemIndex--;
+            rowspan++;
+          }
+          arr[lastItemIndex].rowspan = rowspan;
+        }
+        return item;
+      });
+}
--- backend/routes/table.js
+++ backend/routes/table.js
@@ -1,17 +1,29 @@
-const Pageres = require('pageres');
-const fs = require('fs');
-const baseHtml = fs.readFileSync('./images/table/index.html', 'utf-8');
-const renderHtml = require('../images/table/index.js');
+const PageRes = require('pageres');
+const fs = require('fs/promises');
+const renderHtml = require('../images/table/index');
 
 module.exports = async function routes(fastify) {
   fastify.get('/table.png', (request, reply) => {
+    const theme = request.query.theme || 'dark';
+    const lang = request.query.lang || 'en';
     const path = request.query.path.split('.');
-    import('../../generateGameData/data/' + path[0] + '.js')
-        .then((file) => {
-          const html = renderHtml(baseHtml, file.data[path[1]]);
 
-          new Pageres()
-              .src(`data:text/html,${html}`, ['1024x768'])
+    Promise.all([
+      import('../../generateGameData/data/' + path[0] + '.js'),
+      fs.readFile('./images/table/index.html', 'utf-8'),
+      fs.readFile('./images/table/style.css', 'utf-8'),
+    ])
+        .then((values) => {
+          const html = renderHtml(
+              values[1],
+              values[0].data[path[1]],
+              {theme, lang},
+          );
+
+          new PageRes({
+            css: values[2],
+          })
+              .src(`data:text/html,${html}`, ['1228x431'])
               .run()
               .then((img) => {
                 reply.type('image/png').send(img[0]);
