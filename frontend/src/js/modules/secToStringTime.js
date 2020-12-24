942bce7a817b8c747e4540c53f7468df4f624ac4
Xinos <40886699+userXinos@users.noreply.github.com>
2020-12-24 10:43:30 +0700

    fix time formatter

--- frontend/src/js/modules/secToStringTime.js
+++ frontend/src/js/modules/secToStringTime.js
@@ -2,9 +2,9 @@ export default function(getStr, secIn) {
   let result = '';
   const sec = parseInt(secIn, 10);
 
-  const days = Math.round(sec / 86400);
-  const hours = Math.round((sec - (days * 86400)) / 3600);
-  const minutes = Math.round((sec - (hours * 3600 + days * 86400)) / 60);
+  const days = Math.floor(sec / 86400);
+  const hours = Math.floor((sec - (days * 86400)) / 3600);
+  const minutes = Math.floor((sec - (hours * 3600 + days * 86400)) / 60);
   const seconds = sec - (days * 86400 + hours * 3600 + minutes * 60);
 
   if (days > 0) result += `${days}${getStr('days')}`;
