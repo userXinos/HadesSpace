3d3b9b641604d29fbc1c6bd45cc7b9343f48970e
Xinos <userxinos@gmail.com>
2022-04-10 12:31:05 +0700

    plugins manage

---
+++ src/js/Vue/pluginsConfig.js
@@ -0,0 +1,15 @@
+export default {
+    VueProgressBar: {
+        color: '#345b66',
+        failedColor: '#874b4b',
+        thickness: '3px',
+        transition: {
+            speed: '0.2s',
+            opacity: '0.2s',
+            termination: 300,
+        },
+        autoRevert: true,
+        location: 'top',
+        inverse: false,
+    },
+};
