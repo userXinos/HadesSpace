02cbf754b169cd199bbc957b3de9054caf16f73b
Xinos <userxinos@gmail.com>
2022-06-01 18:50:58 +0700

    collapse content menu

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -2,20 +2,29 @@
   <div class="container">
 
     <template v-if="Object.keys(args.data).length > 1">
-      <h2>{{ $t("CONTENT") }}</h2>
-      <div class="list">
-        <ol>
-          <li
-            v-for="{Name, TID} of args.data"
-            :key="Name"
-          >
-            <a
-              v-t="TID"
-              :href="`#${Name}`"
-            />
-          </li>
-        </ol>
+      <div class="title">
+        <h2>{{ $t('CONTENT') }}</h2>
+        <div
+          class="hamburger-icon"
+          :class="{'opened': contentOpened}"
+          @click="contentOpened = !contentOpened"
+        ><div /></div>
       </div>
+      <transition-group
+        v-if="contentOpened"
+        tag="ol"
+        class="list"
+      >
+        <li
+          v-for="{Name, TID} of args.data"
+          :key="Name"
+        >
+          <a
+            v-t="TID"
+            :href="`#${Name}`"
+          />
+        </li>
+      </transition-group>
     </template>
 
     <v-data
@@ -48,34 +57,74 @@ import VData from './Data.vue';
 export default {
     components: { VData },
     props: {
-        args: {
-            /** @type {args} */
-            type: Object,
-            default: () => ({}),
-            required: true,
-        },
+        args: { type: Object, required: true },
+    },
+    data() {
+        return {
+            contentOpened: false,
+        };
     },
 };
 </script>
 
 <style scoped lang="scss">
+@import "../style/vars";
+
 .container {
-    h2 {
-        text-align: center;
+    .title {
         margin: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 2%;
     }
 
     .list {
+        border: 5px solid #586066;
+        list-style-type: none;
+        columns: 200px auto;
+        font-size: 125%;
         margin: 0 3%;
 
-        ol {
-            border: 5px solid #586066;
-            list-style-type: none;
-            columns: 200px auto;
-            font-size: 125%;
-        }
         li {
             text-align: center;
+        }
+    }
+}
+
+// credits: https://codepen.io/Bilal1909/pen/KKdrmRP
+.hamburger-icon {
+    width: 50px;
+    cursor: pointer;
+
+    @media screen and (max-width: 1000px){
+        width: 40px;
+    }
+
+    &:before, &:after, & div {
+        background: $text-color;
+        content: "";
+        display: block;
+        height: 5px;
+        border-radius: 3px;
+        margin: 7px 0;
+        transition: 0.5s;
+
+        @media screen and (max-width: 1000px){
+            height: 4px;
+            margin: 2px 0;
+        }
+    }
+
+    &.opened {
+        &:before {
+            transform: translateY(12px) rotate(135deg);
+        }
+        &:after {
+            transform: translateY(-12px) rotate(-135deg);
+        }
+        &  div {
+            transform: scale(0);
         }
     }
 }
