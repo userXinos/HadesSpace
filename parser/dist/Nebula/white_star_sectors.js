5bee8d926a1dacee6c428070047d26e2ae89f181
Xinos <userxinos@gmail.com>
2022-06-15 00:22:46 +0700

    new data from Nebula patch #2

--- parser/dist/Nebula/white_star_sectors.js
+++ parser/dist/Nebula/white_star_sectors.js
@@ -5,11 +5,23 @@
  */
 
 export default {
-    Name: ["EntryGate", "HighYieldAsteroids", "LowYieldAsteroids", "HighQualityPlanet", "LowQualityPlanet"],
-    AsteroidsMin: [2, 2, 3, 4, 3],
-    AsteroidsMax: [4, 8, 5, 6, 4],
-    HydrogenPerDay: [160, 3000, 350, 500, 300],
-    AsteroidBeltAxis1Size: [800, 900, 900, 1100, 1100],
-    AsteroidBeltAxis2Size: [1400, 1500, 1600, 1600, 1600],
-    PlanetTypes: [null, null, null, "whitestar1", "whitestar2"],
+    Name: [
+        "EntryGate",
+        "HighYieldAsteroids",
+        "LowYieldAsteroids",
+        "HighQualityPlanet",
+        "LowQualityPlanet",
+        "RefreshAsteroids1",
+        "RefreshAsteroids2",
+    ],
+    AsteroidsMin: [2, 2, 3, 4, 3, 3, 3],
+    AsteroidsMax: [4, 8, 6, 6, 4, 5, 5],
+    HydrogenPerDay: [160, 2000, 350, 700, 400, 1200, 800],
+    AsteroidBeltAxis1Size: [800, 900, 900, 1100, 1100, 900, 900],
+    AsteroidBeltAxis2Size: [1400, 1500, 1600, 1600, 1600, 1600, 1600],
+    CerbGroup: [null, null, null, null, null, null, null],
+    PlanetTypes: [null, null, null, "whitestar1", "whitestar2", null, null],
+    ExtraAsteroidSpawnTick: [null, null, null, null, null, 3200, 6800],
+    ExtraAsteroidsNumber: [null, null, null, null, null, 6, 2],
+    ExtraAsteroidHydroAmount: [null, null, null, null, null, 1200, 800],
 };
