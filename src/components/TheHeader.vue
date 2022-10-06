18e5152453214856aff2d3cf16c451321859a04b
Xinos <userxinos@gmail.com>
2022-10-06 19:46:49 +0700

    hide header on scroll

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,37 +1,45 @@
 <template>
   <div class="offset">
-    <header class="header">
+    <transition>
+      <header
+        v-if="active"
+        class="header"
+      >
 
+        <div class="buttons">
 
-      <div class="buttons">
+          <template v-if="isMinMode">
+            <div
+              class="sidebar"
+              @click="openSidebar"
+            />
+          </template>
 
-        <template v-if="isMinMode">
-          <div
-            class="sidebar"
-            @click="openSidebar"
-          />
-        </template>
+          <template v-else>
+            <navigation />
+            <settings />
+          </template>
 
-        <template v-else>
-          <navigation />
-          <settings />
-        </template>
+        </div>
 
-      </div>
+        <div class="logo">
+          <router-link to="/">
+            <div class="image" />
+          </router-link>
+        </div>
 
-      <div class="logo">
-        <router-link to="/">
-          <div class="image" />
-        </router-link>
-      </div>
-
-    </header>
+      </header>
+    </transition>
   </div>
 </template>
 
 <script>
 import Navigation from './TheNavigation.vue';
 import Settings from '@/components/Settings.vue';
+
+let lastScrollY = null;
+let $tableHeadTarget = null;
+let $modals = null;
 
 export default {
     name: 'Header',
@@ -46,6 +54,37 @@ export default {
             type: Function,
             requested: true,
             default: null,
+        },
+    },
+    data() {
+        return {
+            active: true,
+        };
+    },
+    mounted() {
+        lastScrollY = window.scrollY;
+        $tableHeadTarget = document.querySelector('#table-head-target');
+        $modals = document.querySelector('#modals');
+        window.addEventListener('scroll', this.scroll);
+    },
+    unmounted() {
+        window.removeEventListener('scroll', this.scroll);
+    },
+    methods: {
+        scroll() {
+            const newScrollY = window.scrollY;
+
+            if (newScrollY > lastScrollY && $tableHeadTarget.children.length == 0 && $modals.children.length == 0) {
+                if (this.active) {
+                    this.active = false;
+                }
+            } else {
+                if (!this.active) {
+                    this.active = true;
+                }
+            }
+
+            lastScrollY = newScrollY;
         },
     },
 };
@@ -123,5 +162,13 @@ $mw: 1000px;
     @include header($height: 60px);
 }
 
+.v-enter-active, .v-leave-active {
+    top: 0;
+    transition: ease-in-out, top 500ms
+}
+.v-enter-from, .v-leave-to {
+    top: -100px;
+    transition: ease-in-out, top 500ms;
+}
 
 </style>
