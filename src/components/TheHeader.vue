1ddb79d3091cc28153ef8a11b7446419e1358a5f
Xinos <userxinos@gmail.com>
2022-10-28 08:57:30 +0700

    remove hide header on scroll

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -6,43 +6,39 @@
     <slot />
 
 
-    <transition>
-      <header
-        v-if="active"
-        class="header"
-      >
+    <header
+      class="header"
+    >
 
-        <div class="buttons">
+      <div class="buttons">
 
-          <template v-if="isMinMode">
-            <div
-              class="sidebar"
-              @click="openSidebar"
-            />
-          </template>
+        <template v-if="isMinMode">
+          <div
+            class="sidebar"
+            @click="openSidebar"
+          />
+        </template>
 
-          <template v-else>
-            <navigation />
-            <settings />
-          </template>
+        <template v-else>
+          <navigation />
+          <settings />
+        </template>
 
-        </div>
+      </div>
 
-        <div class="logo">
-          <router-link to="/">
-            <div class="image" />
-          </router-link>
-        </div>
+      <div class="logo">
+        <router-link to="/">
+          <div class="image" />
+        </router-link>
+      </div>
 
-      </header>
-    </transition>
+    </header>
   </div>
 </template>
 
 <script>
 import Navigation from './TheNavigation.vue';
 import Settings from '@/components/Settings.vue';
-import debounce from 'lodash.debounce';
 
 export default {
     name: 'Header',
@@ -57,36 +53,6 @@ export default {
             type: Function,
             requested: true,
             default: null,
-        },
-    },
-    data() {
-        return {
-            active: true,
-        };
-    },
-    created() {
-        this.disable = debounce(() => this.active = !this.active, 250);
-    },
-    mounted() {
-        this.lastScrollY = window.scrollY;
-        this.$tableHeadTarget = document.querySelector('#table-head-target');
-        this.$modals = document.querySelector('#modals');
-        window.addEventListener('scroll', this.scroll);
-    },
-    unmounted() {
-        window.removeEventListener('scroll', this.scroll);
-    },
-    methods: {
-        scroll() {
-            const newScrollY = window.scrollY;
-
-            if (this.$tableHeadTarget.children.length != 0 || this.$modals.children.length != 0) {
-                this.active = true;
-                return;
-            }
-            this.active = newScrollY <= this.lastScrollY;
-
-            this.lastScrollY = newScrollY;
         },
     },
 };
@@ -162,14 +128,6 @@ $mw: 1000px;
 
 @media screen and (max-width: $mw) {
     @include header($height: 60px);
-}
-
-.v-enter-active, .v-leave-active {
-    top: 0;
-}
-.v-enter-from, .v-leave-to {
-    top: -100px;
-    transition: ease-in-out, top 1s;
 }
 
 </style>
