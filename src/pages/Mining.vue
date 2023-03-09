8d875f888d0f0975be1fa366921461d9960661f7
Xinos <40886699+userXinos@users.noreply.github.com>
2023-03-09 22:11:35 +0700

    update assets, fixes

--- src/pages/Mining.vue
+++ src/pages/Mining.vue
@@ -2,7 +2,6 @@
   <ModulePage
     type="Mining"
     portrait="portrait_Asteroids_cr"
-    :post-filter="postFilter"
   />
 </template>
 
@@ -11,21 +10,5 @@ import ModulePage from '@/components/ModulePage.vue';
 
 export default {
     components: { ModulePage },
-    methods: {
-        postFilter(data) {
-            const Genesis = { ...data.Genesis };
-
-            if (Genesis.HydroPerNewAsteroid_YS && Genesis.HydroPerNewAsteroid_WS) {
-                Genesis.HydroPerNewAsteroid_YS = Genesis.HydroPerNewAsteroid_YS
-                    .map((e, i) => e * Genesis.MaxNewAsteroids[i]);
-                Genesis.HydroPerNewAsteroid_WS = Genesis.HydroPerNewAsteroid_WS
-                    .map((e, i) => e * Genesis.MaxNewAsteroids[i]);
-
-                data.Genesis = Genesis;
-            }
-
-            return data;
-        },
-    },
 };
 </script>
