33654ddbd290a367783b33a6bc3ca1bb2ea32af2
Xinos <40886699+userXinos@users.noreply.github.com>
2023-05-19 23:38:54 +0700

    patch note

--- src/regulation/patchCommits.js
+++ src/regulation/patchCommits.js
@@ -2,6 +2,11 @@
 
 export default [
     {
+        hash: 'bfa4fb222211fea8604a31258b797d21c9b44ab3',
+        title: 'Patch № 16.2',
+        note: '[May 19]: ',
+    },
+    {
         hash: 'b3ecca02ed85e5d3a91bf7b34fd13ec52f3febc1',
         title: 'Patch № 16.1',
         note: '[May 15]: RS9 EA; many translations into other languages',
