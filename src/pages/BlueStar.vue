e1965bc4a140b9e2abae905621bc7edd7b2c9b20
Xinos <userxinos@gmail.com>
2024-02-14 21:24:55 +0700

    fixes i18n

--- src/pages/BlueStar.vue
+++ src/pages/BlueStar.vue
@@ -11,7 +11,11 @@
 </template>
 
 <script setup>
+import { useI18n } from 'vue-i18n';
 import Page from '@/components/Page.vue';
 import stars from '@Data/stars.js';
 import img from '@Img/game/portraits/portrait_BlueStar.jpg';
+
+const { t } = useI18n();
+t('TID_BS_FINAL_POSITIONS'); // for i18n-extract
 </script>
