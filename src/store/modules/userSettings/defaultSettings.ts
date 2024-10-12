cb67faa53fe9e0f292973912f3426a3134d69c34
233dada <2474129326@qq.com>
2024-10-12 18:29:12 +0800

    According to browser's language to choose default language

--- src/store/modules/userSettings/defaultSettings.ts
+++ src/store/modules/userSettings/defaultSettings.ts
@@ -1,5 +1,19 @@
+import langData from "@i18n/index.json";
+function extractLangs(data: any): string[]{
+    return Object.values(data).map((locale: any) => locale.Code);
+};
+const langList = extractLangs(langData);
+if(navigator.language === 'zh-CN')
+    {
+        var lan = 'zh-si'
+    }else{
+        var lan = navigator.language
+    };
+if(langList.includes(lan)){}else{
+    var lan = 'en'
+};
 export default Object.freeze(<Settings>{
-    language: 'zh-si',
+    language: lan,
     disableFilters: false,
     compactModulesByArtTypeTable: true,
     planetsCalcSp2: false,
